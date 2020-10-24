0a39023c0be6701ae0cc2a3997701a354c4327c0
Daniel_Why <352812247@qq.com>
2020-10-24 22:00:18 +0800

    kivy_learn ponggame

--- othello/othello_gui.py
+++ othello/othello_gui.py
@@ -7,20 +7,25 @@ from kivy.uix.button import Button
 label_game_name='[font=Lobster-Regular][i]Dinasour[/i][/font] [size=30sp][color=#a7324a][b]Egg[/b][/color][/size]'
 label_creator='[font=OCRAEXT]Fatzero[/font]'
 
-class OthelloApp(App):
-    def build(self):
+class FirstScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.title='Othello'
-
-        layout = GridLayout(cols=1)
+        self.cols = 1
+        # layout = GridLayout(cols=1)
         lab_game_name = Label(text=label_game_name,markup=True,font_size='25sp',size_hint_y=None,height=100)
         lab_creator = Label(text=label_creator,markup=True,font_size='20sp',size_hint_y=None,height=100)
         img_logo = Image(source='.\\othello\\static\\othello.png',size_hint_y=None,height=350)
         lab_start_button=Button(text='START',size_hint_y=None,height=10)
 
-        layout.add_widget(lab_game_name)
-        layout.add_widget(img_logo)
-        layout.add_widget(lab_start_button)
-        layout.add_widget(lab_creator)
-        return layout
-OthelloApp().run()
+        self.add_widget(lab_game_name)
+        self.add_widget(img_logo)
+        self.add_widget(lab_start_button)
+        self.add_widget(lab_creator)
+
+class OthelloApp(App):
+    def build(self):
+        return FirstScreen()
+if __name__ == "__main__":
+    OthelloApp().run()
 
