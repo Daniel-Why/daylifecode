f5f3efbe655f938356a73a30230bec6a727a1009
Daniel-Why <352812247@qq.com>
2024-02-15 17:57:26 +0800

    update change_image_path

--- text_manage/markdown_operate/change_image_path.py
+++ text_manage/markdown_operate/change_image_path.py
@@ -4,7 +4,7 @@ def replace_image_path(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         content = f.read()
     
-    pattern = r'!\[.*?\]\((C:/Users/D2010/AppData/Roaming/Typora/typora-user-images/.*?)\)'
+    pattern = r'C:/Users/D2010/AppData/Roaming/Typora/typora-user-images/.*?'
     new_content = re.sub(pattern, lambda x: x.group(0).replace('C:/Users/D2010/AppData/Roaming/Typora/typora-user-images/', './../assets/article_data/Pman002/pic/'), content)
     
     with open(file_path, 'w', encoding='utf-8') as f:
